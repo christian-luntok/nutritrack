a5be40d6a24e445749d995f531ffd9e0e8aa10a4
Christian <sefluntok@gmail.com>
2023-07-10 20:16:28 +1000

    Remove: Removed unused Components.

--- components/CallToAction/index.js
+++
@@ -1,9 +0,0 @@
-/**
-
-=========================================================
-** CallToAction Exports - Styles can be edit on @stylesheet - styles/core/components/cta
-=========================================================
-
-**/
-
-export * from "@components/CallToAction";
